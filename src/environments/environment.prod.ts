8921a6f8a74e9b47cc1e05d349512f2081e0862a
Stephen Prakash <stephen@examly.in>
2019-08-16 22:43:16 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  HOST: {
+    COOKIE_LINK: 'https://developertoolapi.herokuapp.com',
+    link: 'https://developertoolapi.herokuapp.com',
+    HOST_LINK: 'https://developertoolapi.herokuapp.com',
+  },
+};
